90c7a196778371570607545bead46563591a958d
trainh <traitrantruon@gmail.com>
2022-07-14 00:26:36 +0700

    update details

--- app/src/main/java/com/trainh/assignmentprm/DetailsActivity.java
+++ app/src/main/java/com/trainh/assignmentprm/DetailsActivity.java
@@ -1,14 +1,63 @@
 package com.trainh.assignmentprm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.ActionBar;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.trainh.assignmentprm.entities.Product;
+
+import java.text.DecimalFormat;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    ImageView ivProduct;
+    TextView tvName;
+    TextView tvPrice;
+    TextView tvDescription;
+    Button bntSMS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        DecimalFormat formatter = new DecimalFormat("#,###,###");
+
+        ivProduct = (ImageView) findViewById(R.id.imgProduct);
+        tvName = (TextView) findViewById(R.id.tvdName);
+        tvPrice = (TextView) findViewById(R.id.tvdPrice);
+        tvDescription = (TextView) findViewById(R.id.tvdDescription);
+        bntSMS = (Button) findViewById(R.id.bntSMS);
+
+        Intent intent =getIntent();
+        if(intent.getExtras() != null){
+            Product product = (Product) intent.getSerializableExtra("data");
+            ivProduct.setImageResource(product.getImage());
+            tvName.setText(product.getName());
+            tvPrice.setText(formatter.format(product.getPrice()) + " vnd");
+            tvDescription.setText(String.valueOf(product.getDescription()));
+        }
+
+
+
+        bntSMS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number = "0374192404";
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms", number, null)));
+            }
+        });
     }
+
+    public static void getSmsDetails(String phoneNumber, String SMSBody) {}
 }
--- app/src/main/java/com/trainh/assignmentprm/HomeActivity.java
+++ app/src/main/java/com/trainh/assignmentprm/HomeActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import com.trainh.assignmentprm.adapter.ProductAdapter;
 import com.trainh.assignmentprm.database.Database;
 import com.trainh.assignmentprm.entities.Product;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -68,7 +70,7 @@ public class HomeActivity extends AppCompatActivity implements ProductAdapter.Se
         List<Product> products = new ArrayList<Product>();
         Cursor dataProduct = database.GetData("SELECT * FROM product WHERE type = 'Máy tính'");
         while (dataProduct.moveToNext()) {
-            Product product = new Product(dataProduct.getInt(0), dataProduct.getInt(1), dataProduct.getString(2), dataProduct.getDouble(3), dataProduct.getString(4), dataProduct.getString(5));
+            Product product = new Product(dataProduct.getInt(0), dataProduct.getInt(1), dataProduct.getString(2), dataProduct.getDouble(3), dataProduct.getInt(4) ,dataProduct.getString(5), dataProduct.getString(6));
             Log.d("product", dataProduct.getString(2));
             products.add(product);
         }
@@ -79,7 +81,7 @@ public class HomeActivity extends AppCompatActivity implements ProductAdapter.Se
         List<Product> products = new ArrayList<Product>();
         Cursor dataProduct = database.GetData("SELECT * FROM product WHERE type = 'Bàn phím'");
         while (dataProduct.moveToNext()) {
-            Product product = new Product(dataProduct.getInt(0), dataProduct.getInt(1), dataProduct.getString(2), dataProduct.getDouble(3), dataProduct.getString(4), dataProduct.getString(5));
+            Product product = new Product(dataProduct.getInt(0), dataProduct.getInt(1), dataProduct.getString(2), dataProduct.getDouble(3),dataProduct.getInt(4),  dataProduct.getString(5), dataProduct.getString(6));
             Log.d("product", dataProduct.getString(2));
             products.add(product);
         }
@@ -88,7 +90,12 @@ public class HomeActivity extends AppCompatActivity implements ProductAdapter.Se
 
     @Override
     public void selectedProduct(Product product) {
-        Log.d("selectedProduct", product.getName());
+        Log.d("product", product.getName());
+        Intent intent = new Intent(HomeActivity.this, DetailsActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("data", product);
+        startActivity(intent);
+//        startActivity(new Intent(HomeActivity.this, DetailsActivity.class).putExtra("data",  product));
     }
 
     @Override
