c19ffb7c9afe8996b98c917eeb5b24afc6f35bb4
trainh <traitrantruon@gmail.com>
2022-07-18 10:35:38 +0700

    update thanh toan

--- app/src/main/java/com/trainh/assignmentprm/CartActivity.java
+++ app/src/main/java/com/trainh/assignmentprm/CartActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -41,10 +44,14 @@ public class CartActivity extends AppCompatActivity implements CartAdapter.Selec
         btnThanhToan = (Button) findViewById(R.id.btnthanhtoan);
         LinearLayoutManager linearLayoutManagerCart = new LinearLayoutManager(this);
         linearLayoutManagerCart.setOrientation(LinearLayoutManager.VERTICAL);
-
+        List<Product> products = getProductComputer();
+        int total = 0;
+        for(int i = 0; i < products.size();i++){
+            total += products.get(i).getPrice() * products.get(i).getQuantity();
+        }
         rvCart.setLayoutManager(linearLayoutManagerCart);
         productList = getProductComputer();
-        txtMoney.setText(formatter.format(TongTien()));
+        txtMoney.setText(formatter.format(total));
         cartAdapter = new CartAdapter(productList, this);
         rvCart.setAdapter(cartAdapter);
         cartAdapter.notifyDataSetChanged();
@@ -52,11 +59,43 @@ public class CartActivity extends AppCompatActivity implements CartAdapter.Selec
         btnThanhToan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                dathang();
             }
         });
 
     }
+
+    private void dathang(){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.oder_dialog);
+        Window window = dialog.getWindow();
+        if(window == null){
+            return;
+        }
+
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+
+        Button btncontinues = dialog.findViewById(R.id.bntContinue);
+        btncontinues.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(view.getContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button exit = dialog.findViewById(R.id.btnExit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        dialog.show();
+    }
+
     private List<Product> getProductComputer() {
         List<Product> products = new ArrayList<Product>();
         Cursor dataProduct = database.GetData("SELECT cart.id, product.id, product.image, product.name, product.price, cart.quantity FROM cart INNER JOIN product ON cart.idProduct = product.id");
@@ -66,15 +105,6 @@ public class CartActivity extends AppCompatActivity implements CartAdapter.Selec
             products.add(product);
         }
         return products;
-    }
-
-    private double TongTien(){
-        double total = 0;
-        for(int i = 0; i < productList.size();i++){
-
-            total += productList.get(i).getPrice();
-        }
-        return total;
     }
 
     @Override
